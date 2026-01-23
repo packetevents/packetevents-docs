a2f22a80d7f365b164986cbc2b664f6f8a3b93b9
booky <booky@booky.dev>
2026-01-23 02:21:18 +0100

    Setup branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import {defineConfig} from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    site: "https://packetevents-docs.pages.dev",
+    output: "static",
+    compressHTML: process.env.NODE_ENV === "production",
+    trailingSlash: "ignore",
+    integrations: [
+        starlight({
+            components: {
+                Head: "./src/components/Head.astro"
+            },
+            favicon: "./favicon.ico",
+            title: "packetevents",
+            social: [
+                {icon: "github", label: "GitHub", href: "https://github.com/retrooper/packetevents"},
+                {icon: "discord", label: "Discord", href: "https://discord.com/invite/gtuVwM2ZP2"},
+            ],
+            logo: {
+                src: "./src/assets/logo.webp",
+            },
+            sidebar: [
+                {
+                    label: "Guides",
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        {label: "Example Guide", slug: "guides/example"},
+                    ],
+                },
+                {
+                    label: "Reference",
+                    autogenerate: {directory: "reference"},
+                },
+            ],
+        }),
+    ],
 });
